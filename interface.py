1ba2300a105f969714e93bd5aa5bc6ebbd6cb3b4
Samurai <samurai@ubuntu.ubuntu-domain>
2013-02-04 16:31:22 +0100

    check the status

--- interface.py
+++ interface.py
@@ -8,7 +8,7 @@ class Interface:
       self.nrf = CDC_nRF(dev_path)
       self.nrf.setPower(True) # enable power
       self.nrf._bus.setDTR(True) # enable transparent mode
-      self.nrf._bus.timeout = None # disable read/write timeouts
+      self.nrf._bus.timeout = None # disable read/write timeouts#
 
    def send(self, data):
       self.nrf._bus.write(data)
