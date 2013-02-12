85ceab0009cefbac41009057aa49d8237a1b1d69
Samurai <samurai@ubuntu.ubuntu-domain>
2013-02-12 15:45:21 +0100

    changes to send_recv adding zmq

--- send_recv1.py
+++ send_recv1.py
@@ -3,7 +3,13 @@ from interface import Interface
 from time import sleep
 import serial
 import thread
-from Crypto.Cipher import DES
+import yaml
+import zmq
+import random
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:5556")
 
 i = Interface('/dev/ttyACM0')
 
@@ -13,22 +19,19 @@ def send_msg():
 	global mcounter	
 	global i	
 	mFlag = 1	
-	mType = 'a'
+	mType = 'ab'
 	msenderID = 2
 	mspeed = 40.5	
 	mheading = 90
 	mpayload = 'helworld'
-	des = DES.new('12345678', DES.MODE_ECB)
-	cipher_text = des.encrypt(mpayload)
-	
+		
 	while 1:
-		myMessage = msgpack.packb([mFlag,mType,msenderID,mspeed,mheading,cipher_text,mcounter])
+		myMessage = msgpack.packb([mFlag,mType,msenderID,mspeed,mheading,mpayload,mcounter])
 		i.send(myMessage)
 		mcounter += 1
 		sleep(0.1)
 	
 def receive_msg():
-	des = DES.new('12345678', DES.MODE_ECB)
 	unp = msgpack.Unpacker()
 	global i
 	while 1:
@@ -38,11 +41,12 @@ def receive_msg():
 			if type(msg) is tuple:
 				print msg
 				if len(msg) == 7:
-					print des.decrypt(msg[5])
-				else:
-					print 'garbage type 1', msg 
-			else:
-				print 'garbage type 2', msg 
+					if(msg[1] == 'aa'):
+						socket.send(msg[5])
+			#	else:
+			#		print 'garbage type 1', msg 
+			#else:
+			#	print 'garbage type 2', msg 
 	
 try:
 	thread.start_new_thread(receive_msg, () )
